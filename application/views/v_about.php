e9f7b226c4c2e20b4e4d9d46d5de6b496fe9fa2d
RizqiSetiawan19207036 <setiawanrizqy62@gmail.com>
2022-06-29 10:57:59 +0700

    Tugas Pertemuan 1 sampai 5

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type='disc'>
+ <li>Nama Depan : Rizqi</li>
+ <li>Nama Belakang : Setiawan</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type='none'>
+ <li> Jalan Prumpung Sawah RT 001 RW 004 No.29 Kel.Cipinang Besar Utara Kec.Jatinegara Kota Jakarta Timur, DKI JAKARTA</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type='none'>
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type='square'>
+ <li>Main Game</li>
+ <li>Rebahan</li>
+ </ul>
+</section>
